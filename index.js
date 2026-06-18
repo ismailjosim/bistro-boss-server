a27e1c2367cf40d17e3e9ae0ea4465d19f3dfda6
ismailjosim <ismailjosim99@gmail.com>
2026-06-18 08:22:36 +0600

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -14,44 +14,43 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 const verifyJWT = (req, res, next) => {
-  const authHeader = req?.headers?.authorization
-  if (!authHeader) {
-    return res.status(401).send('Unauthorized access')
-  }
+	const authHeader = req?.headers?.authorization
+	if (!authHeader) {
+		return res.status(401).send('Unauthorized access')
+	}
 
-  const token = authHeader.split(' ')[1]
+	const token = authHeader.split(' ')[1]
 
-  jwt.verify(token, process.env.JWT_TOKEN_SECRET, (error, decoded) => {
-    if (error) {
-      return res.status(403).send({
-        message: 'forbidden access',
-      })
-    }
-    req.decoded = decoded
-    next()
-  })
+	jwt.verify(token, process.env.JWT_TOKEN_SECRET, (error, decoded) => {
+		if (error) {
+			return res.status(403).send({
+				message: 'forbidden access',
+			})
+		}
+		req.decoded = decoded
+		next()
+	})
 }
 
-
-const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.s9x13go.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = process.env.DB_URL
 
 // Database: Client Create
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+})
 
 // Database: Connect with Database
 const dbConnect = async () => {
-  try {
-    client.connect()
-    console.log('Database Connected Successfully✅')
-  } catch (error) {
-    console.log(error.name, error.message)
-  }
+	try {
+		client.connect()
+		console.log('Database Connected Successfully✅')
+	} catch (error) {
+		console.log(error.name, error.message)
+	}
 }
 dbConnect()
 
@@ -65,239 +64,229 @@ const paymentCollection = db.collection('payments')
 
 // routes: Default Route
 app.get('/', (req, res) => {
-  try {
-    res.send('Restaurant Server Is Running 🚩')
-  } catch (error) {
-    console.log(error.name, error.message)
-  }
+	try {
+		res.send('Restaurant Server Is Running 🚩')
+	} catch (error) {
+		console.log(error.name, error.message)
+	}
 })
 
 // post: JWT middleware post request.
 app.post('/jwt', (req, res) => {
-  const user = req.body
-  const token = jwt.sign(user, process.env.JWT_TOKEN_SECRET, {
-    expiresIn: '1d',
-  })
-  res.send({ token })
+	const user = req.body
+	const token = jwt.sign(user, process.env.JWT_TOKEN_SECRET, {
+		expiresIn: '1d',
+	})
+	res.send({ token })
 })
 
 // middleware: verify admin middleware
 const verifyAdmin = async (req, res, next) => {
-  const email = req.decoded.email
-  const query = { email: email }
-  const user = await userCollection.findOne(query)
+	const email = req.decoded.email
+	const query = { email: email }
+	const user = await userCollection.findOne(query)
 
-  if (user?.role !== 'admin') {
-    res.status(403).send({ message: 'Forbidden Access' })
-  }
-  next()
+	if (user?.role !== 'admin') {
+		res.status(403).send({ message: 'Forbidden Access' })
+	}
+	next()
 }
 
 // Route: All menu routes
 app.get('/menu', async (req, res) => {
-  const query = req.query.tags
-  const result = await menuCollection.find(query).toArray()
-  res.send(result)
+	const query = req.query.tags
+	const result = await menuCollection.find(query).toArray()
+	res.send(result)
 })
 
 app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
-  const addData = req.body
-  const result = await menuCollection.insertOne(addData)
-  res.send(result)
+	const addData = req.body
+	const result = await menuCollection.insertOne(addData)
+	res.send(result)
 })
 
 app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
-  const id = req.params.id
-  const query = { _id: new ObjectId(id) }
-  const result = await menuCollection.deleteOne(query)
-  res.send(result)
+	const id = req.params.id
+	const query = { _id: new ObjectId(id) }
+	const result = await menuCollection.deleteOne(query)
+	res.send(result)
 })
 
 app.get('/reviews', async (req, res) => {
-  const query = {}
-  const result = await reviewCollection.find(query).toArray()
-  res.send(result)
+	const query = {}
+	const result = await reviewCollection.find(query).toArray()
+	res.send(result)
 })
 
 // get: cart data
 app.get('/carts', verifyJWT, async (req, res) => {
-  try {
-    const email = req.query.email
-    if (!email) {
-      res.send([])
-    }
-    // check valid user
-    const decodedEmail = req.decoded.email
-    if (email !== decodedEmail) {
-      res.status(403).send({ message: 'Forbidden Access' })
-    }
+	try {
+		const email = req.query.email
+		if (!email) {
+			res.send([])
+		}
+		// check valid user
+		const decodedEmail = req.decoded.email
+		if (email !== decodedEmail) {
+			res.status(403).send({ message: 'Forbidden Access' })
+		}
 
-    const query = { userEmail: email }
-    const result = await cartCollection.find(query).toArray()
-    res.send(result)
-  } catch (error) {
-    console.log(error.message)
-  }
+		const query = { userEmail: email }
+		const result = await cartCollection.find(query).toArray()
+		res.send(result)
+	} catch (error) {
+		console.log(error.message)
+	}
 })
 
 // post: cart data
 app.post('/carts', async (req, res) => {
-  try {
-    const item = req.body
-    const result = await cartCollection.insertOne(item)
-    res.send(result)
-  } catch (error) {
-    console.log(error.message)
-  }
+	try {
+		const item = req.body
+		const result = await cartCollection.insertOne(item)
+		res.send(result)
+	} catch (error) {
+		console.log(error.message)
+	}
 })
 
 // delete: cart data
 app.delete('/carts/:id', async (req, res) => {
-  const id = req.params.id
-  const query = { _id: new ObjectId(id) }
+	const id = req.params.id
+	const query = { _id: new ObjectId(id) }
 
-  const result = await cartCollection.deleteOne(query)
-  res.send(result)
+	const result = await cartCollection.deleteOne(query)
+	res.send(result)
 })
 
 // Get All user Information from database
 app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
-  const query = {}
-  const result = await userCollection.find(query).toArray()
-  res.send(result)
+	const query = {}
+	const result = await userCollection.find(query).toArray()
+	res.send(result)
 })
 
 // Add a new User => Database
 app.post('/users', async (req, res) => {
-  const user = req.body
-  const query = { email: user.email }
-  const existingUser = await userCollection.findOne(query)
-  if (existingUser) {
-    return res.send({ message: 'User Is already Registered!' })
-  } else {
-    const result = await userCollection.insertOne(user)
-    res.send(result)
-  }
+	const user = req.body
+	const query = { email: user.email }
+	const existingUser = await userCollection.findOne(query)
+	if (existingUser) {
+		return res.send({ message: 'User Is already Registered!' })
+	} else {
+		const result = await userCollection.insertOne(user)
+		res.send(result)
+	}
 })
 
 // delete a user From database
 app.delete('/users/:id', async (req, res) => {
-  const id = req.params.id
-  const query = { _id: new ObjectId(id) }
-  const result = await userCollection.deleteOne(query)
-  res.send(result)
+	const id = req.params.id
+	const query = { _id: new ObjectId(id) }
+	const result = await userCollection.deleteOne(query)
+	res.send(result)
 })
 
 // Update:Modify User Admin Role
 app.patch('/users/admin/:id', async (req, res) => {
-  const id = req.params.id
-  const filter = { _id: new ObjectId(id) }
-  const updateDoc = {
-    $set: { role: 'admin' },
-  }
-  const options = { upsert: true }
-  const result = await userCollection.updateOne(filter, updateDoc)
-  res.send(result)
+	const id = req.params.id
+	const filter = { _id: new ObjectId(id) }
+	const updateDoc = {
+		$set: { role: 'admin' },
+	}
+	const options = { upsert: true }
+	const result = await userCollection.updateOne(filter, updateDoc)
+	res.send(result)
 })
-
-// Check isAdmin or not
-// user security layer check
-// 01: verifyJWT
-// 02: similar email
-// 03: check role admin or not.
 
 //? verify Admin role
 app.get('/users/admin/:email', verifyJWT, async (req, res) => {
-  const email = req.params.email
-  if (req.decoded.email !== email) {
-    res.send({ admin: false })
-  }
-  const query = { email: email }
+	const email = req.params.email
+	if (req.decoded.email !== email) {
+		res.send({ admin: false })
+	}
+	const query = { email: email }
 
-  const user = await userCollection.findOne(query)
-  const result = { admin: user?.role === 'admin' }
-  res.send(result)
+	const user = await userCollection.findOne(query)
+	const result = { admin: user?.role === 'admin' }
+	res.send(result)
 })
 
 app.get('/users/admin/:email', async (req, res) => {
-  const email = req.params.email
-  const query = { email: email }
-  const user = await userCollection.findOne(query)
-  const result = { admin: user?.role === 'admin' }
-  res.send(result)
+	const email = req.params.email
+	const query = { email: email }
+	const user = await userCollection.findOne(query)
+	const result = { admin: user?.role === 'admin' }
+	res.send(result)
 })
 
 //* Payment: create payment intent
 app.post('/create-payment-intent', verifyJWT, async (req, res) => {
-  try {
-    const { price } = req.body
-    const amount = price * 100
+	try {
+		const { price } = req.body
+		const amount = price * 100
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      currency: 'usd',
-      amount: amount,
-      payment_method_types: ['card'],
-    })
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    })
-  } catch (error) {
-    res.send({
-      success: false,
-      error: error.message,
-    })
-  }
+		const paymentIntent = await stripe.paymentIntents.create({
+			currency: 'usd',
+			amount: amount,
+			payment_method_types: ['card'],
+		})
+		res.send({
+			clientSecret: paymentIntent.client_secret,
+		})
+	} catch (error) {
+		res.send({
+			success: false,
+			error: error.message,
+		})
+	}
 })
 
 // post payment status to database.
 app.post('/payments', verifyJWT, async (req, res) => {
-  try {
-    const payment = req.body
-    const result = await paymentCollection.insertOne(payment)
-    res.send(result)
-  } catch (error) {
-    res.send({
-      error: error,
-    })
-  }
+	try {
+		const payment = req.body
+		const result = await paymentCollection.insertOne(payment)
+		res.send(result)
+	} catch (error) {
+		res.send({
+			error: error,
+		})
+	}
 })
 
 // get: all payment Info from database
 app.get('/payments', verifyJWT, async (req, res) => {
-  try {
-    const query = {}
-    const payment = await paymentCollection.find(query).toArray()
-    res.send(payment)
-  } catch (error) {
-    res.send(error)
-  }
+	try {
+		const query = {}
+		const payment = await paymentCollection.find(query).toArray()
+		res.send(payment)
+	} catch (error) {
+		res.send(error)
+	}
 })
 
 app.get('/admin-stats', verifyJWT, verifyAdmin, async (req, res) => {
-  try {
-    const customers = await userCollection.estimatedDocumentCount()
-    const products = await menuCollection.estimatedDocumentCount()
-    const orders = await paymentCollection.estimatedDocumentCount()
+	try {
+		const customers = await userCollection.estimatedDocumentCount()
+		const products = await menuCollection.estimatedDocumentCount()
+		const orders = await paymentCollection.estimatedDocumentCount()
 
-    // best way to get sum of a field to use group and sum operator
-    const payments = await paymentCollection.find().toArray()
-    const revenue = payments.reduce((sum, current) => sum + current.price, 0)
+		// best way to get sum of a field to use group and sum operator
+		const payments = await paymentCollection.find().toArray()
+		const revenue = payments.reduce((sum, current) => sum + current.price, 0)
 
-    res.send({
-      revenue,
-      customers,
-      products,
-      orders,
-    })
-  } catch (error) {
-    res.send(error)
-  }
+		res.send({
+			revenue,
+			customers,
+			products,
+			orders,
+		})
+	} catch (error) {
+		res.send(error)
+	}
 })
 
 app.listen(port, () => {
-  console.log(`Bistro boss is sitting on port ${ port }`.bgRed)
+	console.log(`Bistro boss is sitting on port ${port}`.bgRed)
 })
-
-
-
-
